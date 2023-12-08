f943dbc8cd4134a12d8b08145cd3115e9a97c41c
sharifturi <sharif.netroots@gmail.com>
2023-12-08 13:57:58 +0500

    New updates

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
 		<div className="container">
 			<h1>MERN Stack CRUD</h1>
 			<p>
-				<b>Front-end</b>: React.js v17+ with RRDv6+
+				<b>Front-end</b>: React.js with React Router Dom+
 			</p>
 			<p>
 				<b>Back-end</b>: Node.js, Express.js
